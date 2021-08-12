021d3a73cdf3a76a95c3ade529ff554636fe4466
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-12 11:01:32 +0530

    Add onClick event for sign in.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {FacebookOutlined, GoogleOutlined} from "@ant-design/icons";
+import "firebase/app";
+
+import { auth } from "../firebase"; 
+import firebase from 'firebase/app';
 
 const Login = () => {
     return (
         <div id = "login-page">
             <div id="login-card">
                 <h2>Welcome to UniChat</h2>
-                <div className="login-button google">
+                <div className="login-button google"
+                onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                     <GoogleOutlined /> Sign In with Google
                 </div>
                 <br/> <br/>
-                <div className="login-button facebook">
+                <div className="login-button facebook"
+                onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
                     <FacebookOutlined/> Sign In with facebook
                 </div>
             </div>

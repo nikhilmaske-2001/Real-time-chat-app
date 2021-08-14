53757b1bd2e3e1b4f8a01849bcce2bb58a46a14a
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-14 10:45:03 +0530

    Created chat component.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { ChatEngine } from "react-chat-engine";
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
 
 const Chats = () => {
+    const history = useHistory();
+
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        history.push("/");
+    }
     return (
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Let's Chat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+            <ChatEngine
+                height="calc(100vh - 66px)"
+                projectId="1c0f3ab0-e5a0-434f-8258-4b6ec6653416"
+                userName="."
+                userScreen="."
+            />
+        </div>
     );
 };
 

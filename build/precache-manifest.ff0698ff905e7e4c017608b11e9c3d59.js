d31c004cf74c5262473dd65f8d1034437e0aedb0
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-14 17:56:32 +0530

    Add build.

---
+++ build/precache-manifest.ff0698ff905e7e4c017608b11e9c3d59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f09c70330527ae6e1aa91868fa1ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74eb309b4e7c8710ed8",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "ec76f52d1b6a2a9876b9",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "f74eb309b4e7c8710ed8",
+    "url": "/static/js/2.1805c234.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.1805c234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec76f52d1b6a2a9876b9",
+    "url": "/static/js/main.3056deaf.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
